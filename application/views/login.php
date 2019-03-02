ccea569f1c0de9b4a7dbf0d33857c0da8248889f
yasin sama <work@i-quall.com>
2019-03-02 16:08:43 +0530

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -25,50 +25,50 @@
 		</ul>
 		<div class="tabs-container">
 			<!-- Login -->
-                        
-                            <?php if($this->session->flashdata('success')){?>
-                                 <div class="success">
-                            <div class="alert alert-success alert-dismissable">
-                              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                              <?= $this->session->flashdata('success') ?>
-                            </div>
-                            </div>
-                            <?php } ?>
-                        
-                        <?php if($this->session->flashdata('error')){?>
-                                 <div class="success">
-                            <div class="alert alert-danger alert-dismissable">
-                              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                              <?= $this->session->flashdata('error') ?>
-                            </div>
-                            </div>
-                            <?php } ?>
-                        
-			<div class="tab-content" id="tab1"  >
+
+			<?php if($this->session->flashdata('success')){?>
+			<div class="success">
+				<div class="alert alert-success alert-dismissable">
+					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+					<?= $this->session->flashdata('success') ?>
+				</div>
+			</div>
+			<?php } ?>
+
+			<?php if($this->session->flashdata('error')){?>
+			<div class="success">
+				<div class="alert alert-danger alert-dismissable">
+					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+					<?= $this->session->flashdata('error') ?>
+				</div>
+			</div>
+			<?php } ?>
+
+			<div class="tab-content" id="tab1">
 
 				<h3 class="margin-bottom-10 margin-top-10">Login</h3>
 				<form method="post" class="login" role="form">
 					<div class="form-group">
-                        <label>Username or Email Address :</label>
-                        <input class="form-control" name="username" id="username" type="text" required>
-                    </div>
+						<label>Username or Email Address :</label>
+						<input class="form-control" name="username" id="username" type="text" required>
+					</div>
 					<div class="form-group">
-                        <label>Password :</label>
-                        <input type="password" name="password" id="password" class="form-control" required>
-                    </div>
+						<label>Password :</label>
+						<input type="password" name="password" id="password" class="form-control" required>
+					</div>
 					<div class="form-group">
 						<input type="submit" class="button" name="login" value="Login" />
 						<label for="rememberme" class="rememberme">
-						<input name="rememberme" type="checkbox" id="rememberme" value="forever" /> Remember Me</label>
-                    </div>
+							<input name="rememberme" type="checkbox" id="rememberme" value="forever" /> Remember Me</label>
+					</div>
 					<p class="lost_password">
-						<a href="#" >Lost Your Password?</a>
+						<a href="#">Lost Your Password?</a>
 					</p>
 				</form>
 			</div>
 
-				<!-- Register -->
-				
+			<!-- Register -->
+
 		</div>
 	</div>
 </div>
